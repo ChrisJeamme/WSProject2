c55aa0ec01700a23abb3a84e9cb4ef7e97f7448b
Chris <chrisjahim@free.fr>
2019-01-11 05:56:11 +0100

    fix: gitignore problems

--- target/classes/static/js/regions_functions.js
+++
@@ -1,8 +0,0 @@
-const editButtonPress = function(button)
-{
-    let id_row = button.id.split("button_")[1];
-    let regionName = document.querySelector("#row_"+id_row+" .regionNameField").innerHTML;
-
-    document.querySelector("#formEditionId").value = id_row;
-    document.querySelector("#formRegionName").value = regionName;
-}
