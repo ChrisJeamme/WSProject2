934c003088b9a2b0de2c61588f24c8ce9e3062f0
KHAMED Smaïl <khameds@gmail.com>
2019-01-12 14:24:14 +0100

    Fix, in order to compile (else Job class is missing etc)

--- src/main/java/com/projetws/model/SchoolClassRepository.java
+++ src/main/java/com/projetws/model/SchoolClassRepository.java
@@ -9,6 +9,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 public interface SchoolClassRepository extends PagingAndSortingRepository<SchoolClass, Long>
 {
 	public List<SchoolClass> findAll();
-//	public List<Country> findByCountryName(String countryName);
-//	public Country findByCountryId(String id);
+	
 }
