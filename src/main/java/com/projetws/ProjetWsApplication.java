ad40211122965759ea17067aa654d2e4b3faa3f2
KHAMED Smaïl <khameds@gmail.com>
2019-01-16 02:35:14 +0100

    Define MultipartFile properties, add Bean in Project's Main to file system storage (not used i the end)

--- src/main/java/com/projetws/ProjetWsApplication.java
+++ src/main/java/com/projetws/ProjetWsApplication.java
@@ -1,9 +1,11 @@
 package com.projetws;
 
-import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import com.projetws.tools.StorageService;
 
 @SpringBootApplication
 public class ProjetWsApplication
@@ -12,5 +14,13 @@ public class ProjetWsApplication
 	{
 		SpringApplication.run(ProjetWsApplication.class, args);
 	}
+	/*
+	  @Bean
+	    CommandLineRunner init(StorageService storageService) {
+	        return (args) -> {
+	            //storageService.deleteAll();
+	            storageService.init();
+	        };
+	    }*/
 }
 
