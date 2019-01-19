10d9b5b5135bfb1a3b72ccd0373bfc09b2a253d3
Chris <chrisjahim@free.fr>
2019-01-19 15:05:10 +0100

    sec:authorize work

--- src/main/java/com/projetws/WebSecu.java
+++ src/main/java/com/projetws/WebSecu.java
@@ -15,6 +15,7 @@
 package com.projetws;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -22,6 +23,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 
 import com.projetws.model.UserService;
 /**
@@ -33,7 +35,12 @@ import com.projetws.model.UserService;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecu extends WebSecurityConfigurerAdapter
 {
-
+	@Bean
+    public SpringSecurityDialect springSecurityDialect()
+	{
+        return new SpringSecurityDialect();
+    }
+	
 	/**
 	 *
 	 * @param http
