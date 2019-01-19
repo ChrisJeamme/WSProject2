49fcb7d0d9b690b43160b57fe77380d58069d3f1
Chris <chrisjahim@free.fr>
2019-01-19 02:15:47 +0100

    push test

--- src/main/java/com/projetws/controller/UploadController.java
+++ src/main/java/com/projetws/controller/UploadController.java
@@ -42,7 +42,7 @@ public class UploadController {
 
 	@Autowired
 	private SchoolClassRepository schoolClassRepository;
-	
+	 
 	@ApiOperation(value="Store a Photo into the database" , httpMethod="POST", response= UploadPhotoResponse.class)
 	@PostMapping("/upload")
 	public UploadPhotoResponse uploadFile(@ApiParam(value="Image file", required=false) @RequestParam("file") MultipartFile file,
