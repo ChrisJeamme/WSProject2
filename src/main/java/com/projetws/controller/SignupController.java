99d4e0576719c742f9c8d028bfb6d541bd806656
Chris <chrisjahim@free.fr>
2019-01-20 02:15:49 +0100

    modify the controller to have 2 type of signup

--- src/main/java/com/projetws/controller/SignupController.java
+++ src/main/java/com/projetws/controller/SignupController.java
@@ -1,5 +1,7 @@
 package com.projetws.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import com.projetws.model.User;
 import com.projetws.model.UserRepository;
@@ -20,63 +24,88 @@ import io.swagger.annotations.Api;
 @Controller
 public class SignupController
 {
+	private static final Logger logger = LogManager.getLogger(SignupController.class);
 	@Autowired
 	UserService userService;
 	@Autowired
 	UserRepository userRepository;
 	
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public String inscription(
-            @RequestParam("username") String userName,
+    public String signup(
+    		HttpServletRequest request,
+            @RequestParam("userName") String userName,
             @RequestParam("firstName") String firstName,
             @RequestParam("lastName") String lastName,
             @RequestParam("phoneNumber") String phoneNumber,
             @RequestParam("mail") String mail,
             @RequestParam("password") String password,
             @RequestParam("password2") String password2,
+            @RequestParam("type") String type,
             Model m
     )
     {
-	if (password.compareTo(password2) != 0) //The 2 passwords are different
+    	logger.info("Signup asked");
+    	if (password.compareTo(password2) != 0) //The 2 passwords are different
         {
-            m.addAttribute("error", "Les mots de passes sont différents");
-            return "forward:/signup";
+        	logger.warn("Passwords are differents");
+            m.addAttribute("error", "Passwords are differents");
+            return "redirect:"+request.getHeader("Referer");
         }
         
         if (!InputDataVerification.emailAddressOk(mail))
         {
-            m.addAttribute("error", "L'adresse e-mail n'est pas valide");
-            return "forward:/signup";
+        	logger.warn("The email adress is not valid");
+            m.addAttribute("error", "The email adress is not valid");
+            return "redirect:"+request.getHeader("Referer");
         }
-
+    	
         List<String> roles = new ArrayList<>();
-        roles.add("ROLE_DEFAULT");
-        roles.add("ROLE_SCHOOLADMIN");
-        
+    	if(type == "schoolAdmin")
+    	{
+            roles.add("ROLE_DEFAULT");
+            roles.add("ROLE_SCHOOLADMIN");
+    	}
+    	else if(type == "photographer")
+    	{
+            roles.add("ROLE_DEFAULT");
+            roles.add("ROLE_PHOTOGRAPHER");
+		}
+    	else
+    	{
+    		logger.error("signupType="+type);
+        	logger.warn("Unknown type of signup");
+            m.addAttribute("error", "Error in the form");
+    		return "redirect:"+request.getHeader("Referer");
+    	}
+    	
         int add = userService.addUser(new User(mail, firstName, lastName, phoneNumber, password, userName, roles)); //Try to add the user
 
-        if (add == 1)	//The pseudo is already used
+        if (add == 1)
         {
-            m.addAttribute("error", "Ce pseudo est déjà utilisé");
-            return "forward:/signup";
+        	logger.warn("Username already used");
+            m.addAttribute("error", "Username already used");
+            return "redirect:"+request.getHeader("Referer");
         }
-        if (add == 2)	//The mail is already used
+        if (add == 2)
         {
-            m.addAttribute("mailExists", "Il y a déjà un compte avec cet email");
-            return "forward:/signup";
+        	logger.warn("Mail already used");
+            m.addAttribute("mailExists", "Mail already used");
+            return "redirect:"+request.getHeader("Referer");
         }
-        m.addAttribute("user", userName);
-        m.addAttribute("mdp", password);
-        return "/login"; //The user has been correctly added in the database
+        logger.info("Sign up success : username="+userName);
+        m.addAttribute("app_username", userName);
+        m.addAttribute("app_password", password);
+        return "redirect:/login"; //The user has been correctly added in the database
     }
 
-    /**
-     * Request signup
-     * @return The template "signup"
-     */
-    @RequestMapping("/signup")
-    public String page()
+    @RequestMapping("/signupSchoolAdministrator")
+    public String signupSchoolAdministrator()
     {
-        return "/signup";
+        return "/signupSchoolAdministrator";
+    }
+    @RequestMapping("/signupPhotographer")
+    public String signupPhotographer()
+    {
+        return "/signupPhotographer";
     }
 }
