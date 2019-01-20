5fbf18683c318c1f2f9bda60bdf8803c5e6ee2b5
Chris <chrisjahim@free.fr>
2019-01-20 05:03:55 +0100

    add redirection and form for school creation

--- src/main/java/com/projetws/controller/SchoolManagementController.java
+++ src/main/java/com/projetws/controller/SchoolManagementController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.projetws.model.School;
 import com.projetws.model.SchoolClass;
@@ -32,6 +33,13 @@ public class SchoolManagementController
 	@Autowired
 	SchoolClassRepository schoolClassRepository;
 
+	@RequestMapping("/newSchool")
+	public String newSchool(@RequestParam("schoolName") String schoolName)
+	{
+		logger.info("new school = "+schoolName);
+		return "redirect:/schoolManagement";
+	}
+	
 	@RequestMapping("/schoolManagement")
 	public String getSchoolManagement(Principal principal, Model m)
 	{
@@ -42,7 +50,7 @@ public class SchoolManagementController
 		}
 
 		String userName = principal.getName();
-		logger.debug(userName);
+		logger.info(userName);
 		
 		if(!userRepository.existsByUserName(userName))
 		{
@@ -53,8 +61,8 @@ public class SchoolManagementController
 		User manager = userRepository.findByUserName(userName);
 		if(!schoolRepository.existsByManager(manager))
 		{
-			logger.error("School not found");
-			return "redirect:/error";
+			logger.info("School not found");
+			return "schoolCreation";
 		}
 		School school = schoolRepository.findByManager(manager);
 		List<SchoolClass> schoolClasses = schoolClassRepository.findAllBySchool(school);
