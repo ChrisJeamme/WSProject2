672f37b038c61e7ce952b914e8ae8b8d9ab80e19
Chris <chrisjahim@free.fr>
2019-01-21 17:10:07 +0100

    add void class to test SecurityTools

---
+++ src/test/java/com/projetws/tools/SecurityToolsTest.java
@@ -0,0 +1,52 @@
+package com.projetws.tools;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SecurityToolsTest {
+
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+
+	@Test
+	public void testHasRole()
+	{
+		
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGenerateRolesWith()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsConnected()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEmailNotUsedVerification()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSamePage()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayErrorAndRedirect()
+	{
+		fail("Not yet implemented");
+	}
+
+}
